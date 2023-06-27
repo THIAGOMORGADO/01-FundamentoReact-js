83c587d3a5c7bed63f8b1b7697b88c0b4eff27d3
thiago morgado <thiago.n.morgado@gmail.com>
2023-06-27 10:35:33 -0300

    altered

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import styles from'./Header.module.css'
 export function Header() {
   return(
     <header className={styles.header}>
-      <p>rola!</p>
+      <p>rola!   </p>
     </header>
     
   )
