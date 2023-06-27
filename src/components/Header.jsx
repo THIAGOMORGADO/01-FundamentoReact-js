6452e778e05db29106b1790758209638e283f195
thiago morgado <thiago.n.morgado@gmail.com>
2023-06-27 10:22:47 -0300

    criando build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 import styles from'./Header.module.css'
 
-import igniteLogo from '../assets/ignite-logo.svg'
+import igniteLogo from '../../public/Ignite-logo.svg'
 
 export function Header() {
   return(
