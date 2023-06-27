c8345c319c34ec822c65d00495fef682c9e23eb0
thiago morgado <thiago.n.morgado@gmail.com>
2023-06-27 10:34:10 -0300

    feat:/Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,7 @@ import styles from'./Header.module.css'
 export function Header() {
   return(
     <header className={styles.header}>
-      <img src={'/Ignite-logo.svg'} alt='logotop'/>
-
+      <p>rola!</p>
     </header>
     
   )
