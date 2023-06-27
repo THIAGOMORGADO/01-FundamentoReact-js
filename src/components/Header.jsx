7e006f9ace168eee62dfd9ddce9ac7c8487c25c1
thiago morgado <thiago.n.morgado@gmail.com>
2023-06-27 10:29:14 -0300

    alterde

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 import styles from'./Header.module.css'
 
-import igniteLogo from '../../public/Ignite-logo.svg'
+import igniteLogo from '/Ignite-logo.svg'
 
 export function Header() {
   return(
